fcc82a7224e77978f7a7567f5fbe56942f7f3b69
naval88 <navalkishor2005@gmail.com>
2024-03-22 23:15:35 +0530

    First Commit

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+
+  render() {
+    return <div>
+      <h1>Employee Crud Example In React Js</h1>
+    </div>
+  }
+}
+
+export default Main;
